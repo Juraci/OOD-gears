5bf4e18f33028828925021bce721080e0051c192
Juraci <juraci.vieira@gmail.com>
2015-06-27 18:45:30 -0300

    Rephrasing everyone of Gear's methods as a question ought to make sense

--- lib/gears/gear.rb
+++ lib/gears/gear.rb
@@ -1,4 +1,5 @@
 class Gear
+  # Please Mr. Gear, what are your tire (size)? is just downright rediculous
   attr_reader :chainring, :cog, :rim, :tire
 
   def initialize(chainring, cog, rim, tire)
@@ -8,10 +9,12 @@ class Gear
     @tire = tire
   end
 
+  # Please MR. Gear what is your ratio? seems perfectly resonable
   def ratio
     chainring / cog.to_f
   end
 
+  # Please Mr. Gear, what are your gear_inches? is kind weird
   def gear_inches
     # tire goes around rim twice for diameter
     ratio * (rim + (tire * 2))
